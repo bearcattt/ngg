f3ecb82eeac1f98dd10d817d7ac4db701645d14c
bearcattt <ewigglet8@gmail.com>
2024-11-27 16:24:03 -0500

    Update embed.js

--- static/embed.js
+++ static/embed.js
@@ -4,7 +4,7 @@ const iframe = document.getElementById("frame");
 
 registerSW()
   .then(() => {
-    iframe.src = "/nowgg/hvtrs8%2F-mctjsqpmt%2Ccmm-";  // Properly terminate this line with a semicolon
+    iframe.src = "/static/nowgg/hvtrs8%2F-mctjsqpmt%2Ccmm-";  // Properly terminate this line with a semicolon
   }) // Close the then block here
   .catch((err) => {
     alert(`Encountered error:\n${err}`);
