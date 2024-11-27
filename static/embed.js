487f60ac3a0417cc964250d61d06377409259459
bearcattt <ewigglet8@gmail.com>
2024-11-27 17:53:33 -0500

    Update embed.js

--- static/embed.js
+++ static/embed.js
@@ -1,11 +1,17 @@
 "use strict";
-
-const iframe = document.getElementById("frame");
+let destination = "";
+const gameframe = document.getElementById("frame");
+try {
+  destination = new URL(location.hash.slice(1)).toString();
+} catch (err) {
+  alert(`Bad # string or bad URL. Got error:\n${err}`);
+  throw err;
+}
 
 registerSW()
   .then(() => {
-    iframe.src = "/static/nowgg/hvtrs8%2F-mctjsqpmt%2Ccmm-";  // Properly terminate this line with a semicolon
-  }) // Close the then block here
+    gameframe.src = (__uv$config.prefix + __uv$config.encodeUrl(destination));
+  })
   .catch((err) => {
     alert(`Encountered error:\n${err}`);
   });
